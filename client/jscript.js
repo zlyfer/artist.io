7b64ee59f42d247a34cb10611e28e7d54054701e
Frederik Shull <zlyfer@pm.me>
2018-11-30 22:37:09 +0100

    scorelist fix

--- client/jscript.js
+++ client/jscript.js
@@ -193,7 +193,7 @@ window.onload = function() {
 				});
 			}
 			scorelist.sort(function(a, b) {
-				return b[1] - a[1];
+				return b.score - a.score;
 			});
 			for (let i = 0; i < 3; i++) {
 				if (scorelist[i]) {
